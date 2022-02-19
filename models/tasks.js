d24f32ddf6f707d64d7989dd86ab9174866c2da2
Ashu0126 <91365712+Ashu0126@users.noreply.github.com>
2022-02-20 01:34:06 +0530

    Add files via upload

--- models/tasks.js
+++ models/tasks.js
@@ -1,1 +1,17 @@
+const mongoose = require("mongoose");
 
+const schema = mongoose.Schema;
+
+const taskSchema = new schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const tasks = mongoose.model("tasks", taskSchema);
+module.exports = tasks;
